1b995048d712bc35494c320ce1025dd265ede399
adrianpaesani <adrian.paesani@gmail.com>
2017-02-14 00:10:17 -0300

    update

--- prices_extension_gasol/models.py
+++ prices_extension_gasol/models.py
@@ -15,18 +15,16 @@ class on_change_function(models.Model):
     Charge2 = fields.Float('Charge2')
     Charge3 = fields.Float('Charge3')
     Charge4 = fields.Float('Charge4')
+		
+	@api.multi
+	@api.onchange('ListPrice','Discount1','Discount2','Discount3','Discount4','Charge1','Charge2','Charge3','Charge4')
+	def on_change_price(self):
 
-    #This method will be called when either the field ListPrice or any of the discount or charge changes.
-    def on_change_price(self,cr,user,ids,ListPrice,Discount1,Discount2,Discount3,Discount4,Charge1,Charge2,Charge3,Charge4,context=None):
-    #Calculate the total
         subtotal = (ListPrice * (1-(Discount1/100))) * (1-(Discount2/100)) * (1-(Discount3/100)) * (1-(Discount4/100)) * (1+(Discount1/100)) * (1+(Charge1/100)) * (1+(Charge2/100)) * (1+(Charge3/100)) * (1+(Charge4/100))
-        res = {
-            'value': {
-        #This sets the total price on the field standard_price.
-                'standard_price': subtotal
-        }
-    }
-        #Return the values to update it in the view.
-        return res
+		values = {
+			'standard_price': subtotal,
+		}
+
+		self.update(values)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
