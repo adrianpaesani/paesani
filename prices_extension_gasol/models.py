1ffe2a7c535e9e327347fd643987c810d703bfad
adrianpaesani <adrian.paesani@gmail.com>
2017-02-13 23:45:45 -0300

    corrected errors

--- prices_extension_gasol/models.py
+++ prices_extension_gasol/models.py
@@ -15,7 +15,7 @@ class on_change_function(models.Model):
     Charge2 = fields.Float('Charge2')
     Charge3 = fields.Float('Charge3')
     Charge4 = fields.Float('Charge4')
-	
+
     #This method will be called when either the field CostPrice or the field ShippingCost changes.
     def on_change_price(self,cr,user,ids,ListPrice,Discount1,Discount2,Discount3,Discount4,Charge1,Charge2,Charge3,Charge4,context=None):
     #Calculate the total
@@ -24,7 +24,9 @@ class on_change_function(models.Model):
             'value': {
         #This sets the total price on the field standard_price.
                 'standard_price': subtotal
-          }
+        }
     }
     #Return the values to update it in the view.
     return res
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
