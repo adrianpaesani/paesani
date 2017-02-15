3884230547aea0e0d85b1486d943c69fe6b9d3ba
adrianpaesani <adrian.paesani@gmail.com>
2017-02-14 23:24:35 -0300

    test

--- prices_extension_gasol/models.py
+++ prices_extension_gasol/models.py
@@ -6,7 +6,7 @@ class on_change_function(models.Model):
     #Inhertis the model product.template
     _inherit = 'product.template'
     #Creates new fields (ListPrice, Discount1, Discount2, Discount3, Discount4, Charge1, Charge2, Charge3, Charge4) in the model product.template
-    list_price = fields.Float('List Price')
+    list_price_def = fields.Float('List Price')
     discount1 = fields.Float('Discount1')
     discount2 = fields.Float('Discount2')
     discount3 = fields.Float('Discount3')
@@ -17,10 +17,10 @@ class on_change_function(models.Model):
     charge4 = fields.Float('Charge4')
 		
     @api.multi
-    @api.onchange('list_price','discount1','discount2','discount3','discount4','charge1','charge2','charge3','charge4')
+    @api.onchange('list_price_def','discount1','discount2','discount3','discount4','charge1','charge2','charge3','charge4')
     def on_change_price(self):
 
-        subtotal = (list_price * (1-(discount1/100))) * (1-(discount2/100)) * (1-(discount3/100)) * (1-(discount4/100)) * (1+(charge1/100)) * (1+(charge2/100)) * (1+(charge3/100)) * (1+(charge4/100))
+        subtotal = (list_price_def * (1-(discount1/100))) * (1-(discount2/100)) * (1-(discount3/100)) * (1-(discount4/100)) * (1+(charge1/100)) * (1+(charge2/100)) * (1+(charge3/100)) * (1+(charge4/100))
         values = {
             'standard_price': subtotal,
         }
